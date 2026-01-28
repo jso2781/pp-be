a65ea04d2fa1ff8ca04c3556355b35f8ee2d2498
yi7829 <yi7829@naver.com>
2026-01-28 17:03:21 +0900

    fix : [전문가 회원 전환 신청] 시퀀스명 변경

--- src/main/resources/database/drugsafe_exprtApply.sql
+++ src/main/resources/database/drugsafe_exprtApply.sql
@@ -15,7 +15,7 @@ INSERT INTO kids_own.tb_pp_m_inst_task
 (brno, task_se_cd, mbr_no, use_yn, rgtr_id, reg_dt, mdfr_id, mdfcn_dt)
 VALUES('9999999999', 'SYS2', NULL, 'Y', NULL, NULL, NULL, NULL);
 
-CREATE SEQUENCE kids_own.tb_pp_m_exprt_task_seq
+CREATE SEQUENCE kids_own.sq_pp_m_exprt_task_sn_1
     INCREMENT BY 1
     MINVALUE 1
     MAXVALUE 9223372036854775807
