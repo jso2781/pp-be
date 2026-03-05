aadf2f3e14bfc7d1eb1f292b1a59812f2155bff9
JOSEONGOK <jso2781@gmail.com>
2026-03-05 15:25:06 +0900

    DUR 정보 검색, 내가 먹는 약의 DUR 정보 조회시 사용할 테이블 인덱스 추가

---
+++ src/main/resources/database/create_dur_indexs.sql
@@ -0,0 +1,67 @@
+-- 대국민포털_DUR병용금기기본(인덱스 1)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_conc_bann_1
+ON kids_own.tb_pp_m_dur_conc_bann(lower(igrd_nm_1));
+
+-- 대국민포털_DUR병용금기기본(인덱스 2)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_conc_bann_2
+ON kids_own.tb_pp_m_dur_conc_bann(lower(igrd_nm_2));
+
+-- 대국민포털_DUR병용금기기본(인덱스 3)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_conc_bann_3
+ON kids_own.tb_pp_m_dur_conc_bann(lower(mfds_igrd_nm_cn_1));
+
+-- 대국민포털_DUR병용금기기본(인덱스 4)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_conc_bann_4
+ON kids_own.tb_pp_m_dur_conc_bann(lower(mfds_igrd_nm_cn_2));
+
+-- 대국민포털_DUR연령금기기본(인덱스 1)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_age_bann_1
+ON kids_own.tb_pp_m_dur_age_bann(lower(igrd_nm));
+
+-- 대국민포털_DUR연령금기기본(인덱스 2)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_age_bann_2
+ON kids_own.tb_pp_m_dur_age_bann(lower(mfds_igrd_nm_cn));
+
+-- 대국민포털_DUR임부금기기본 인덱스
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_prgnt_bann_1
+ON kids_own.tb_pp_m_dur_prgnt_bann(lower(igrd_nm));
+
+-- 대국민포털_DUR용량주의기본(인덱스 1) 
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_cpct_1
+ON kids_own.tb_pp_m_dur_cpct(lower(gnrl_nm));
+
+-- 대국민포털_DUR용량주의기본(인덱스 2) 
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_cpct_2
+ON kids_own.tb_pp_m_dur_cpct(lower(mfds_igrd_nm_cn));
+
+-- 대국민포털_DUR투여기간주의기본(인덱스 1) 
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_dosage_1
+ON kids_own.tb_pp_m_dur_dosage(lower(gnrl_nm));
+
+-- 대국민포털_DUR투여기간주의기본(인덱스 2) 
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_dosage_2
+ON kids_own.tb_pp_m_dur_dosage(lower(mfds_igrd_nm_cn));
+
+-- 대국민포털_DUR효능군중복기본(인덱스 1)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_eftgrp_1
+ON kids_own.tb_pp_m_dur_eftgrp(lower(gnrl_nm));
+
+-- 대국민포털_DUR효능군중복기본(인덱스 2)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_eftgrp_2
+ON kids_own.tb_pp_m_dur_eftgrp(lower(mfds_igrd_nm_cn));
+
+-- 대국민포털_DUR노인주의기본(인덱스 1)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_snctz_1
+ON kids_own.tb_pp_m_dur_snctz(lower(igrd_nm));
+
+-- 대국민포털_DUR노인주의기본(인덱스 2)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_snctz_2
+ON kids_own.tb_pp_m_dur_snctz(lower(mfds_igrd_nm_cn));
+
+-- 대국민포털_DUR수유부주의기본(인덱스 1)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_nursw_1
+ON kids_own.tb_pp_m_dur_nursw(lower(igrd_nm));
+
+-- 대국민포털_DUR수유부주의기본(인덱스 2)
+CREATE INDEX CONCURRENTLY idx_tb_pp_m_dur_nursw_2
+ON kids_own.tb_pp_m_dur_nursw(lower(mfds_igrd_nm_cn));
