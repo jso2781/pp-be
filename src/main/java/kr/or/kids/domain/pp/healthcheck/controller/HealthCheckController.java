7a75f909d9b6e036d078313e19d482ca0b652789
yiKim <yi7829@naver.com>
2026-03-10 17:33:48 +0900

    테스트용 파일관리 API 수정

--- src/main/java/kr/or/kids/domain/pp/healthcheck/controller/HealthCheckController.java
+++ src/main/java/kr/or/kids/domain/pp/healthcheck/controller/HealthCheckController.java
@@ -1,5 +1,25 @@
 package kr.or.kids.domain.pp.healthcheck.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import kr.or.kids.domain.ca.common.file.service.FileService;
 import kr.or.kids.domain.ca.common.file.vo.FileDataReqVO;
 import kr.or.kids.domain.ca.common.file.vo.FileDeleteReqVO;
@@ -10,125 +30,134 @@ import kr.or.kids.global.system.common.ApiResultCode;
 import kr.or.kids.global.system.common.vo.ApiPrnDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ContentDisposition;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/pp/healthcheck")
 public class HealthCheckController {
-    private final FileService fileService;
-    private final Environment environment;
+	
+	private final FileService fileService;
+	private final Environment environment;
 
-    @GetMapping
-    public ResponseEntity<ApiPrnDto> healthcheck() {
-        ApiPrnDto result = new ApiPrnDto(ApiResultCode.SUCCESS);
-        result.setMsg("SUCCESS");
-        ApiResultCode resultCode = ApiResultCode.fromCode(result.getCode());
-        return ResponseEntity.status(resultCode.getHttpStatus()).body(result);
-    }
+	@GetMapping
+	public ResponseEntity<ApiPrnDto> healthcheck() {
+		ApiPrnDto result = new ApiPrnDto(ApiResultCode.SUCCESS);
+		result.setMsg("SUCCESS");
+		ApiResultCode resultCode = ApiResultCode.fromCode(result.getCode());
+		return ResponseEntity.status(resultCode.getHttpStatus()).body(result);
+	}
 
-    @PostMapping(value = "/fileUpload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ApiPrnDto> fileUpload(
-            @RequestPart(value = "file", required = false) MultipartFile file) {
-        ApiPrnDto result = new ApiPrnDto(ApiResultCode.SUCCESS);
-        result.setMsg("FAIL");
+	@PostMapping(value = "/fileInfos")
+	public ResponseEntity<ApiPrnDto> getFileInfos(@RequestBody FileDataReqVO fdrv) {
+		ApiPrnDto result = new ApiPrnDto(ApiResultCode.SUCCESS);
+		result.setMsg("FAIL");
 
-        if (environment.acceptsProfiles(Profiles.of("local", "localout", "dev"))) {
+		if (!environment.acceptsProfiles(Profiles.of("local", "localout", "dev"))) {
+			return null;
+		}
 
-            if (ObjectUtils.isEmpty(file)) {
-                throw new ApplicationException("error.param.required", new String[] {"File"});
-            } else {
-                MultipartFile[] attachFileArr = new MultipartFile[]{file};
+		if (StringUtils.isNotBlank(fdrv.getAtchFileGroupId())) {
+			if (StringUtils.isBlank(fdrv.getTaskSeCd())) {
+				fdrv.setTaskSeCd("pp");
+			}
+			if (StringUtils.isBlank(fdrv.getTaskSeTrgtId())) {
+				fdrv.setTaskSeTrgtId("2");
+			}			
+			
+			HashMap<String, Object> data = new HashMap<>();		
+			data.put("fileInfo", fileService.list(fdrv, 1, 10));		
+			result.setData(data);
+			result.setMsg("SUCCESS");
+		}
 
-                FileGroupInsertReq fgir = new FileGroupInsertReq();
-                fgir.setTaskSeCd("pp");
-                fgir.setTaskSeTrgtId("2");
-                fgir.setRgtrId("admin");
-                fgir.setMdfrId("admin");
+		ApiResultCode resultCode = ApiResultCode.fromCode(result.getCode());
+		return ResponseEntity.status(resultCode.getHttpStatus()).body(result);
+	}
 
-                ApiPrnDto groupInsertResult = fileService.groupInsert(fgir);
+	@PostMapping(value = "/fileUpload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<ApiPrnDto> fileUpload(@RequestPart(value = "file", required = false) MultipartFile file) {
+		ApiPrnDto result = new ApiPrnDto(ApiResultCode.SUCCESS);
+		result.setMsg("FAIL");
 
-                Object atchFileGroupIdObj = groupInsertResult.getData().get("atchFileGroupId");
-                String atchFileGroupId = String.valueOf(atchFileGroupIdObj);
+		if (environment.acceptsProfiles(Profiles.of("local", "localout", "dev"))) {
 
-                HashMap<String, Object> params = new HashMap<>();
-                params.put("savePath", "pp");
-                params.put("atchFileGroupId", atchFileGroupId);
-                params.put("prvcInclYn", "0");
-                params.put("isExcel", "0");
+			if (ObjectUtils.isEmpty(file)) {
+				throw new ApplicationException("error.param.required", new String[] { "File" });
+			} else {
+				MultipartFile[] attachFileArr = new MultipartFile[] { file };
 
-                ApiPrnDto fileResult = fileService.uploadFiles(params, attachFileArr);
+				FileGroupInsertReq fgir = new FileGroupInsertReq();
+				fgir.setTaskSeCd("pp");
+				fgir.setTaskSeTrgtId("2");
+				fgir.setRgtrId("admin");
+				fgir.setMdfrId("admin");
 
-                if("0".equals(fileResult.getCode())){
-                    result.setMsg("[SUCCESS] atchFileGroupId : " + atchFileGroupId);
-                }
-            }
-        }
+				ApiPrnDto groupInsertResult = fileService.groupInsert(fgir);
 
-        ApiResultCode resultCode = ApiResultCode.fromCode(result.getCode());
-        return ResponseEntity.status(resultCode.getHttpStatus()).body(result);
-    }
+				Object atchFileGroupIdObj = groupInsertResult.getData().get("atchFileGroupId");
+				String atchFileGroupId = String.valueOf(atchFileGroupIdObj);
 
-    @PostMapping(value="/fileDownload")
-    public ResponseEntity<Resource> fileDownload(@RequestBody FileDataReqVO fdrv)
-    {
-        if (!environment.acceptsProfiles(Profiles.of("local", "localout", "dev"))) {
-            return null;
-        }
+				HashMap<String, Object> params = new HashMap<>();
+				params.put("savePath", "pp");
+				params.put("atchFileGroupId", atchFileGroupId);
+				params.put("prvcInclYn", "0");
+				params.put("isExcel", "0");
 
-        FileDownResVO downloadParam = fileService.downloadFile(fdrv);
+				ApiPrnDto fileResult = fileService.uploadFiles(params, attachFileArr);
+				
+				Object obj = fileResult.getData().get("uploadList");
 
-        Resource resource = downloadParam.getResource();
+				if ("0".equals(fileResult.getCode())) {
+					result.setMsg("[SUCCESS] atchFileGroupId : " + atchFileGroupId);
+				}
+			}
+		}
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentDisposition(
-                ContentDisposition.attachment()
-                        .filename(downloadParam.getFilename(), StandardCharsets.UTF_8)
-                        .build()
-        );
-        headers.add(HttpHeaders.CONTENT_TYPE, downloadParam.getContentType());
-        headers.add(HttpHeaders.CONTENT_LENGTH, String.valueOf(downloadParam.getContentLength()));
+		ApiResultCode resultCode = ApiResultCode.fromCode(result.getCode());
+		return ResponseEntity.status(resultCode.getHttpStatus()).body(result);
+	}
 
-        log.info("File download started: {}, size: {} bytes", downloadParam.getFilename(), downloadParam.getContentLength());
+	@PostMapping(value = "/fileDownload")
+	public ResponseEntity<Resource> fileDownload(@RequestBody FileDataReqVO fdrv) {
+		if (!environment.acceptsProfiles(Profiles.of("local", "localout", "dev"))) {
+			return null;
+		}
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(resource);
-    }
+		FileDownResVO downloadParam = fileService.downloadFile(fdrv);
 
-    @PostMapping(value="/fileDelete")
-    public ResponseEntity<ApiPrnDto> fileDelete(@RequestBody FileDataReqVO fdrv)
-    {
-        ApiPrnDto result = new ApiPrnDto(ApiResultCode.SUCCESS);
-        result.setMsg("FAIL");
+		Resource resource = downloadParam.getResource();
 
-        if (!environment.acceptsProfiles(Profiles.of("local", "localout", "dev"))) {
-            return null;
-        }
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentDisposition(
+				ContentDisposition.attachment().filename(downloadParam.getFilename(), StandardCharsets.UTF_8).build());
+		headers.add(HttpHeaders.CONTENT_TYPE, downloadParam.getContentType());
+		headers.add(HttpHeaders.CONTENT_LENGTH, String.valueOf(downloadParam.getContentLength()));
 
-        if (StringUtils.isNotBlank(fdrv.getAtchFileGroupId())) {
-            FileDeleteReqVO fileDeleteReqVO = new FileDeleteReqVO();
-            fileDeleteReqVO.setAtchFileGroupId(fdrv.getAtchFileGroupId());
-            fileService.deleteGroupFiles(fileDeleteReqVO);
+		log.info("File download started: {}, size: {} bytes", downloadParam.getFilename(),
+				downloadParam.getContentLength());
 
-            result.setMsg("SUCCESS");
-        }
+		return ResponseEntity.ok().headers(headers).body(resource);
+	}
 
-        ApiResultCode resultCode = ApiResultCode.fromCode(result.getCode());
-        return ResponseEntity.status(resultCode.getHttpStatus()).body(result);
-    }
+	@PostMapping(value = "/fileDelete")
+	public ResponseEntity<ApiPrnDto> fileDelete(@RequestBody FileDataReqVO fdrv) {
+		ApiPrnDto result = new ApiPrnDto(ApiResultCode.SUCCESS);
+		result.setMsg("FAIL");
+
+		if (!environment.acceptsProfiles(Profiles.of("local", "localout", "dev"))) {
+			return null;
+		}
+
+		if (StringUtils.isNotBlank(fdrv.getAtchFileGroupId())) {
+			FileDeleteReqVO fileDeleteReqVO = new FileDeleteReqVO();
+			fileDeleteReqVO.setAtchFileGroupId(fdrv.getAtchFileGroupId());
+			fileService.deleteGroupFiles(fileDeleteReqVO);
+
+			result.setMsg("SUCCESS");
+		}
+
+		ApiResultCode resultCode = ApiResultCode.fromCode(result.getCode());
+		return ResponseEntity.status(resultCode.getHttpStatus()).body(result);
+	}
 }
