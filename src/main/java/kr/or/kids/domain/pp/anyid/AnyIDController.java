dd9a5f282700a36284c60207b82a7b50acb6bc5f
jso2781 <jso2781@hanmail.net>
2026-03-14 02:20:51 +0900

    anyid 변경사항 추가

--- src/main/java/kr/or/kids/domain/pp/anyid/AnyIDController.java
+++ src/main/java/kr/or/kids/domain/pp/anyid/AnyIDController.java
@@ -16,65 +16,65 @@ import kr.or.anyid.auth.AnyidAuth;
 @RestController
 @RequestMapping("/api/pp/auth")
 public class AnyIDController {
-	
-	@GetMapping("/init")
-	void init(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		Map<String, Object> result = new HashMap<String, Object>();
-		
-		AnyidAuth anyidcert = new AnyidAuth();
-		
-		response.setContentType("application/json");
-		response.setCharacterEncoding("utf-8");
-		
-		String txId = request.getParameter("tx");
-		
-		result.put("cfg", "/".equalsIgnoreCase(request.getContextPath()) ? "" : request.getContextPath() + "/config/config.anyidc.json");
-		result.put("txId", txId);
-		result.put("tag", txId);
-		result.put("lvl", 3);
-		result.put("bypass", 0);
-		result.put("theme", "4.2.2");
-		result.put("toggle", true);
-		
-		anyidcert.writeValueAsString(response.getWriter(), result);
-	}
-	
-	@GetMapping("/sso")
-	void sso(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		Map<String, Object> result = new HashMap<String, Object>();
-	
-		AnyidAuth anyidcert = new AnyidAuth();
-	
-		response.setContentType("application/json");
-		response.setCharacterEncoding("utf-8");
-	
-		result.put("mt", request.getParameter("mt"));
-		result.put("portalJoinUri", request.getParameter("portalJoinUri"));
-		result.put("ssoByPass", 0);
-		result.put("agencyContextPath", ("/".equalsIgnoreCase(request.getContextPath()) ? "" : request.getContextPath()));
-	
-		anyidcert.writeValueAsString(response.getWriter(), result);
-	}
-	
-	@GetMapping("/userinfo")
-	void userinfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		Map<String, Object> result = new HashMap<String, Object>();
-	
-		AnyidAuth anyidcert = new AnyidAuth();
-	
-		response.setContentType("application/json");
-		response.setCharacterEncoding("utf-8");
-	
-		HttpSession session = request.getSession();
-		
-		if(null != session) {
-			Map<String, Object> anyidSession = (Map<String, Object>)session.getAttribute("anyid");
-			if(null != anyidSession) {
-				Map<String, Object> userInfo = (Map<String, Object>)anyidSession.get("sso");
-				result.putAll(userInfo);
-			}
-		}
-		
-		anyidcert.writeValueAsString(response.getWriter(), result);
-	}
+
+    @GetMapping("/anyid/init")
+    void init(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map<String, Object> result = new HashMap<String, Object>();
+        
+        AnyidAuth anyidcert = new AnyidAuth();
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        
+        String txId = request.getParameter("tx");
+        
+        result.put("cfg", "/".equalsIgnoreCase(request.getContextPath()) ? "" : request.getContextPath() + "/config/config.anyidc.json");
+        result.put("txId", txId);
+        result.put("tag", txId);
+        result.put("lvl", 3);
+        result.put("bypass", 0);
+        result.put("theme", "4.2.2");
+        result.put("toggle", true);
+        
+        anyidcert.writeValueAsString(response.getWriter(), result);
+    }
+    
+    @GetMapping("/anyid/sso")
+    void sso(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map<String, Object> result = new HashMap<String, Object>();
+    
+        AnyidAuth anyidcert = new AnyidAuth();
+    
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+    
+        result.put("mt", request.getParameter("mt"));
+        result.put("portalJoinUri", request.getParameter("portalJoinUri"));
+        result.put("ssoByPass", 0);
+        result.put("agencyContextPath", ("/".equalsIgnoreCase(request.getContextPath()) ? "" : request.getContextPath()));
+    
+        anyidcert.writeValueAsString(response.getWriter(), result);
+    }
+
+    @GetMapping("/anyid/userinfo")
+    void userinfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map<String, Object> result = new HashMap<String, Object>();
+    
+        AnyidAuth anyidcert = new AnyidAuth();
+    
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+    
+        HttpSession session = request.getSession();
+        
+        if(null != session) {
+            Map<String, Object> anyidSession = (Map<String, Object>)session.getAttribute("anyid");
+            if(null != anyidSession) {
+                Map<String, Object> userInfo = (Map<String, Object>)anyidSession.get("sso");
+                result.putAll(userInfo);
+            }
+        }
+        
+        anyidcert.writeValueAsString(response.getWriter(), result);
+    }
 }
