d1d318297d536e6a42d64d177b65f7c8f2e64961
yiKim <yi7829@naver.com>
2026-04-01 17:32:13 +0900

    fix : 코드레이 수정

--- src/main/java/kr/or/kids/domain/pp/anyid/AnyIdAuthService.java
+++ src/main/java/kr/or/kids/domain/pp/anyid/AnyIdAuthService.java
@@ -108,7 +108,6 @@ public class AnyIdAuthService {
 
             apiPrnDto.setData(bizData);
         }catch(Exception e){
-            e.printStackTrace();
 
             apiPrnDto = new ApiPrnDto(ApiResultCode.SYSTEM_ERROR);
             apiPrnDto.setMsg(e.getMessage());
@@ -141,7 +140,6 @@ public class AnyIdAuthService {
         }
         catch(Exception e){
             log.debug("AnyIdAuthService customDecryptSsob anyidCertRef.decryptSsob after exception : \n{}", e.getMessage());
-            e.printStackTrace();
         }
 
         System.setOut(originalOut);
@@ -180,7 +178,6 @@ public class AnyIdAuthService {
         }
         catch(Exception e){
             log.debug("AnyIdAuthService getCiFromSsob anyidCertRef.decryptSsob after exception : \n{}", e.getMessage());
-            e.printStackTrace();
         }
 
         System.setOut(originalOut);
@@ -222,7 +219,6 @@ public class AnyIdAuthService {
         }
         catch(Exception e){
             log.debug("AnyIdAuthService getUserInfoFromSsob anyidCertRef.decryptSsob after exception : \n{}", e.getMessage());
-            e.printStackTrace();
         }
 
         System.setOut(originalOut);
